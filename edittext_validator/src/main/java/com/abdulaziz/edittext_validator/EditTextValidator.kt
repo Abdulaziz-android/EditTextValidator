67d36814b50e58cbf50334d31bb5f441ea455f1d
Abdulaziz <yoqubjonovabdulaziz@gmail.com>
2021-11-26 11:03:05 +0500

    change

--- edittext_validator/src/main/java/com/abdulaziz/edittext_validator/EditTextValidator.kt
+++
@@ -1,9 +0,0 @@
-package com.abdulaziz.edittext_validator
-
-import android.content.Context
-import android.util.AttributeSet
-
-class EditTextValidator(context: Context, attrs: AttributeSet): androidx.appcompat.widget.AppCompatEditText(context, attrs) {
-
-
-}
